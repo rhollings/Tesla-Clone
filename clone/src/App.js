80d3c78f3470a5e4d626b2a4dcb4a94eec5e111d
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-06-21 21:28:49 +0200

    Add files via upload

---
+++ clone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header.js"
+import Home from "./components/Home.js"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
